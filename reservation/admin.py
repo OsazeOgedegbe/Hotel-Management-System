793028a6929ed0b2465096945ee1133169f7cb05
OsazeOgedegbe <140389715+OsazeOgedegbe@users.noreply.github.com>
2023-07-24 12:16:57 +0100

    Delete reservation directory

--- reservation/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Reservation, ServiceReservation
-from django_cron.models import CronJobLog
-
-
-admin.site.register(Reservation)
-admin.site.register(ServiceReservation)
